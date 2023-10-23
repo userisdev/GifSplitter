c3d890135c559c9cf012778610388ef950bbc5dc
Kento Ooiwa <lunatic.heros@gmail.com>
2023-10-23 23:48:05 +0900

    ImageProcessorを使った実装を追加

---
+++ GifSplitter.v2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageProcessor;
+using ImageProcessor.Imaging;
+using ImageProcessor.Imaging.Formats;
+
+namespace GifSplitter.v2
+{
+    internal class Program
+    {
+        /// <summary> Runs the specified path. </summary>
+        /// <param name="path"> The path. </param>
+        private static void Run(string path)
+        {
+            if (!File.Exists(path))
+            {
+                Console.WriteLine($"not found. : [{path}]");
+                return;
+            }
+
+            FileInfo imageFileInfo = new FileInfo(path);
+            string saveDirPath = Path.Combine(imageFileInfo.DirectoryName, Path.GetFileNameWithoutExtension(imageFileInfo.Name));
+            DirectoryInfo saveDirInfo = new DirectoryInfo(saveDirPath);
+            saveDirInfo.Create();
+
+            using (var image = Image.FromFile(imageFileInfo.FullName))
+            {
+
+                var gd = new GifDecoder(image);
+                Console.WriteLine($"loop count : {gd.LoopCount}");
+                for(int i = 0;i < gd.FrameCount; i++)
+                {
+                    var frame = gd.GetFrame(image, i);
+
+                    var savePath = Path.Combine(saveDirInfo.FullName, $"{i:0000}.png");
+                    frame.Image.Save(savePath);
+                }
+
+            }
+
+
+                Console.WriteLine($"saved. : [{saveDirInfo.Name}]");
+        }
+
+        static void Main(string[] args)
+        {
+            if (!args.Any())
+            {
+                Console.WriteLine("few args.");
+                Environment.Exit(1);
+            }
+
+            string path = args.FirstOrDefault();
+            if (!File.Exists(path) && !Directory.Exists(path))
+            {
+                Console.WriteLine("not found.");
+                Environment.Exit(1);
+            }
+
+            if (File.Exists(path))
+            {
+                FileInfo imageFileInfo = new FileInfo(path);
+                if (!imageFileInfo.Exists || !imageFileInfo.Extension.Equals(".gif", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("args error.");
+                    Environment.Exit(1);
+                }
+
+                Run(imageFileInfo.FullName);
+            }
+
+            if (Directory.Exists(path))
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(path);
+                FileInfo[] files = dirInfo.GetFiles("*", SearchOption.TopDirectoryOnly).Where(e => e.Extension.Equals(".gif", StringComparison.OrdinalIgnoreCase)).ToArray();
+                if (!files.Any())
+                {
+                    Console.WriteLine("not found.");
+                    Environment.Exit(1);
+                }
+
+                foreach (FileInfo fileInfo in files)
+                {
+                    Run(fileInfo.FullName);
+                }
+            }
+
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ GifSplitter.v2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GifSplitter.v2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GifSplitter.v2")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3dd0e23f-0fd6-4fac-a151-f7f49d0c8ffc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
